2106971dd0ce06b3f34335afed67c66480b8742f
wisewhyforge <wisewhyforge@users.noreply.github.com>
2020-07-10 09:20:10 -0500

    Update Q83: Remove Duplicates from a Sorted  List.java

--- Q83: Remove Duplicates from a Sorted  List.java
+++ Q83: Remove Duplicates from a Sorted  List.java
@@ -1,6 +1,6 @@
 /*
-Runtime: 4 ms, faster than 33.56% of Java online submissions for Remove Duplicates from Sorted List.
-Memory Usage: 41.8 MB, less than 5.06% of Java online submissions for Remove Duplicates from Sorted List.
+Runtime: 1 ms, faster than 33.56% of Java online submissions for Remove Duplicates from Sorted List.
+Memory Usage: 39 MB, less than 77.05% of Java online submissions for Remove Duplicates from Sorted List.
 
 Time: 5-10 minutes
 
@@ -21,18 +21,19 @@ The only trick is to rectify the pointers in order to delete a node.
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
+        //Set to keep track of distinct numbers in list
         HashSet<Integer> set = new HashSet<Integer>();
+        //Previous ListNode to keep track of the previous pointer and to link that to the pointer .next in order to delete.
         ListNode previous = null;
+        //Current Pointer to keep track of current location
         ListNode pointer = head;
+        //Loops through the pointer 
         while(pointer != null){
+            //If there is a duplicate that should be deleted then set the previous's pointer to the ucrrent pointer's next and the current pointer to the pointer's next
             if(!set.add(pointer.val)){
-                if(previous == null){
-                    head = pointer.next;
-                    pointer = head;
-                }else{
-                    previous.next = pointer.next;
-                    pointer = pointer.next;
-                }
+                previous.next = pointer.next;
+                pointer = pointer.next;
+            //Otherwise traverse through the list.
             }else{
                 previous = pointer;
                 pointer = pointer.next;
